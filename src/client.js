a55330dc9900f5601932c91875b18670be5c8bf3
Abramstyle <abram.style@gmail.com>
2018-03-01 10:46:23 +0800

    feat: change HMR api to latest react-hot-loader api

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { AppContainer } from 'react-hot-loader';
+// import { AppContainer } from 'react-hot-loader';
 // AppContainer is a necessary wrapper component for HMR
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -11,15 +11,13 @@ import App from './containers/App';
 
 const render = () => {
   ReactDOM.hydrate(
-    <AppContainer>
-      <Provider store={store}>
-        <Router>
-          <App
-            state={preloadedState}
-          />
-        </Router>
-      </Provider>
-    </AppContainer>,
+    <Provider store={store}>
+      <Router>
+        <App
+          state={preloadedState}
+        />
+      </Router>
+    </Provider>,
     document.getRootNode(),
   );
 };
@@ -27,8 +25,8 @@ const render = () => {
 render();
 
 // Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept(() => {
-    render();
-  });
-}
+// if (module.hot) {
+//   module.hot.accept(() => {
+//     render();
+//   });
+// }
