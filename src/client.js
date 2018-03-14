19bf4b270b7292eccf58c8ae4f9f5d7e466f4693
Abramstyle <abram.style@gmail.com>
2018-03-14 14:24:41 +0800

    feat: enable hot reloadable

--- src/client.js
+++ src/client.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Loadable from 'react-loadable';
+import { hot } from 'react-hot-loader';
 
 // import { AppContainer } from 'react-hot-loader';
 // AppContainer is a necessary wrapper component for HMR
@@ -11,15 +12,20 @@ import { Provider } from 'react-redux';
 import routes from './routes';
 import store from './store';
 // import App from './containers/App';
+const App = () => (
+  <Router>
+    {renderRoutes(routes)}
+  </Router>
+);
+const HotReloadableApp = hot(module)(App);
+
 
 const render = () => {
   // Loadable.preloadReady().then(() => {
   Loadable.preloadReady().then(() => {
     ReactDOM.hydrate(
       <Provider store={store}>
-        <Router>
-          {renderRoutes(routes)}
-        </Router>
+        <HotReloadableApp />
       </Provider>,
       document.querySelector('#root'),
     );
