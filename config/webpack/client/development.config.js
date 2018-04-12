71e118ae0ba8e1667ac1e5158902c8e5c9d70dfd
abramstyle <abram.style@gmail.com>
2018-04-12 20:45:02 +0800

    feat: use ionicons locally

--- config/webpack/client/development.config.js
+++ config/webpack/client/development.config.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const path = require('path');
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const resolvepath = path.resolve(__dirname, '../../src/components');
 
 const getConfig = () => ({
   entry: {
@@ -17,26 +16,39 @@ const getConfig = () => ({
       test: /node_modules.*\.css$/,
       use: [{
         loader: 'style-loader',
-        options: {
-          useable: true,
-        },
       }, {
         loader: 'css-loader',
         options: {
-          sourceMap: true,
+          // sourceMap: true,
           minimize: false,
           modules: false,
           importLoaders: 1,
           localIdentName: '[local]',
         },
       }],
+    }, {
+      test: /\.css$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'style-loader',
+      }, {
+        loader: 'css-loader',
+        options: {
+          sourceMap: true,
+          minimize: true,
+          modules: true,
+          importLoaders: 1,
+          localIdentName: '[name]__[local]___[hash:base64:5]',
+        },
+      }, {
+        loader: 'postcss-loader',
+        options: {
+          config: {
+            path: path.resolve(__dirname, '../postcss.config.js'),
+          },
+        },
+      }],
     }],
-  },
-  resolve: {
-    alias: {
-      $components: path.resolve(__dirname, '../../../src/components'),
-      $containers: path.resolve(__dirname, '../../../src/containers'),
-    },
   },
 
   plugins: [
--- config/webpack/client/production.config.js
+++ config/webpack/client/production.config.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+const path = require('path');
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const getConfig = () => ({
   entry: {
@@ -10,6 +12,46 @@ const getConfig = () => ({
       '@abramstyle/redux-api',
     ],
   },
+  module: {
+    rules: [{
+      test: /node_modules.*\.css$/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader,
+      }, {
+        loader: 'css-loader',
+        options: {
+          // sourceMap: true,
+          minimize: false,
+          modules: false,
+          importLoaders: 1,
+          localIdentName: '[local]',
+        },
+      }],
+    }, {
+      test: /\.css$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader,
+      }, {
+        loader: 'css-loader',
+        options: {
+          sourceMap: true,
+          minimize: true,
+          modules: true,
+          importLoaders: 1,
+          localIdentName: '[name]__[local]___[hash:base64:5]',
+        },
+      }, {
+        loader: 'postcss-loader',
+        options: {
+          config: {
+            path: path.resolve(__dirname, '../postcss.config.js'),
+          },
+        },
+      }],
+    }],
+  },
+
   plugins: [
     // new BundleAnalyzerPlugin(),
   ],
